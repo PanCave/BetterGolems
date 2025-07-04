6944fdc9bdd0dcc3851760cf236b122c09479ffe
Julius Freiny <julius.freiny@gmail.com>
2025-07-05 01:59:25 +0200

    Applied fabric mod template

---
+++ src/client/kotlin/com/pancave/bettergolems/BetterGolemsClient.kt
@@ -0,0 +1,9 @@
+package com.pancave.bettergolems
+
+import net.fabricmc.api.ClientModInitializer
+
+object BetterGolemsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
